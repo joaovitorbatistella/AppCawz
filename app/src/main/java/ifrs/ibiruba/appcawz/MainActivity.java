859dcfc9880f1d9064a02da8298cbf34235c792b
joaovitorbatistella <joaovitor.batistella.ifrs@gmail.com>
2019-10-11 11:58:05 -0300

    aula ultima

--- app/src/main/java/ifrs/ibiruba/appcawz/MainActivity.java
+++ app/src/main/java/ifrs/ibiruba/appcawz/MainActivity.java
@@ -1,13 +1,15 @@
 package ifrs.ibiruba.appcawz;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button acesso;
+    TextView txtNoticias;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,10 +17,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         acesso = findViewById(R.id.btn_aceesolivre);
+        txtNoticias = findViewById(R.id.txtNoticias);
 
 
-        Intent it = new Intent(this, cadastro_not_jog.class);
-        startActivity(it);
 
+       // Intent it = new Intent(this, cadastro_not_jog.class);
+      //  startActivity(it);
+
+    }
+
+    public void testeHTTP(View view){
+        ReqHttp requisicao = new ReqHttp("https://cawz.000webhostapp.com/consulta_noticia.php", "titulo=teste&corpo_noticia=teste com espaco&dia=10/10/2019");
+
+       // txtNoticias.setText(requisicao.resposta);
     }
 }
---
+++ app/src/main/java/ifrs/ibiruba/appcawz/ReqHttp.java
@@ -0,0 +1,166 @@
+package ifrs.ibiruba.appcawz;
+
+import android.os.AsyncTask;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class ReqHttp {
+
+    String parametros;
+    String resposta = null;
+    TextView txtResposta;
+
+    public ReqHttp(String url, String parametros) {
+        this.parametros = parametros;
+        new MakeNetworkCall().execute(url, "Post");
+    }
+
+    InputStream ByPostMethod(String ServerURL) {
+
+        InputStream DataInputStream = null;
+        try {
+
+            //Post parameters
+            String PostParam = parametros;
+
+            //Preparing
+            URL url = new URL(ServerURL);
+
+            HttpURLConnection cc = (HttpURLConnection)
+                    url.openConnection();
+            //set timeout for reading InputStream
+            cc.setReadTimeout(5000);
+            // set timeout for connection
+            cc.setConnectTimeout(5000);
+            //set HTTP method to POST
+            cc.setRequestMethod("POST");
+            //set it to true as we are connecting for input
+            cc.setDoInput(true);
+            //opens the communication link
+            cc.connect();
+
+            //Writing data (bytes) to the data output stream
+            DataOutputStream dos = new DataOutputStream(cc.getOutputStream());
+            dos.writeBytes(PostParam);
+            //flushes data output stream.
+            dos.flush();
+            dos.close();
+
+            //Getting HTTP response code
+            int response = cc.getResponseCode();
+
+            //if response code is 200 / OK then read Inputstream
+            //HttpURLConnection.HTTP_OK is equal to 200
+            if(response == HttpURLConnection.HTTP_OK) {
+                DataInputStream = cc.getInputStream();
+            }
+
+        } catch (Exception e) {
+           // Log.e(LOG_TAG, "Error in GetData", e);
+        }
+        return DataInputStream;
+
+    }
+
+    String ConvertStreamToString(InputStream stream) {
+
+        InputStreamReader isr = new InputStreamReader(stream);
+        BufferedReader reader = new BufferedReader(isr);
+        StringBuilder response = new StringBuilder();
+
+        String line = null;
+        try {
+
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+
+        } catch (IOException e) {
+            //Log.e(LOG_TAG, "Error in ConvertStreamToString", e);
+        } catch (Exception e) {
+           // Log.e(LOG_TAG, "Error in ConvertStreamToString", e);
+        } finally {
+
+            try {
+                stream.close();
+            } catch (IOException e) {
+               // Log.e(LOG_TAG, "Error in ConvertStreamToString", e);
+
+            } catch (Exception e) {
+                //Log.e(LOG_TAG, "Error in ConvertStreamToString", e);
+            }
+        }
+        return response.toString();
+
+
+    }
+
+    @Override
+    public String toString() {
+        return "ReqHttp{" +
+                ", resposta='" + resposta + '\'' +
+                '}';
+    }
+
+    public void DisplayMessage(String a) {
+
+       // TxtResult = findViewById(R.id.response);
+       // TxtResult.setText(a);
+       // this.resposta = a;
+
+
+        System.out.println(a);
+    }
+
+
+
+    private class MakeNetworkCall extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            DisplayMessage("Please Wait ...");
+        }
+
+        @Override
+        protected String doInBackground(String... arg) {
+
+            InputStream is = null;
+            String URL = arg[0];
+           // Log.d(LOG_TAG, "URL: " + URL);
+            String res = "";
+
+
+            if (arg[1].equals("Post")) {
+
+                is = ByPostMethod(URL);
+
+            }
+
+            if (is != null) {
+                res = ConvertStreamToString(is);
+            } else {
+                res = "Something went wrong";
+            }
+            return res;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            DisplayMessage(result);
+            //Log.d(LOG_TAG, "Result: " + result);
+           // return result;
+        }
+    }
+}
